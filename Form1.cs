db4c46b6ce4952fb17578a649962e4c274ba10c3
viktoriia <vikto@LAPTOP-08JQHRKQ>
2023-11-28 10:38:10 +0100

    Calculator do oddania

--- Form1.cs
+++ Form1.cs
@@ -7,12 +7,8 @@ namespace Calculator
     public partial class Form1 : Form
     {
         public bool isNumber = false;
-        public string number = "";
         public double number1 = 0;
-        public double number2 = 0;
         public string operation = "";
-        public string nextOperation = "";
-        public bool isOperation = false;
         public double result;
         public Form1()
         {
@@ -30,44 +26,30 @@ namespace Calculator
         {
             Button button = (Button)sender;
             //Klik na cyfry
-            if (isNumber==false)
+            if (textBox1.Text == "0" || isNumber)
             {
                 textBox1.Text = button.Text;
-                number = textBox1.Text;
-                isNumber = true;
+                isNumber = false;
             }
             else
             {
                 textBox1.Text += button.Text;
-                number = textBox1.Text;
             }
-            number1 = double.Parse(textBox1.Text);
+            
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
             //Klik na +,-,/,x
-            //do_operation();
             Button button = (Button)sender;
-            operation = button.Text;
-            do_operation();
+            string nextOperation = button.Text;
+            do_operation(nextOperation);
         }
-            public void do_operation()
+            public void do_operation(string nextOperation)
             {
-               /* if (number1+number2 == 0) {
-                number1 = double.Parse(number);
-                number = "";
-            }
-            else {
-                result = number1;
-                number1 = double.Parse(number); 
-            }
-              */
-              //  number1 = double.Parse(textBox1.Text);
-              result = number1;
-            if ((number1==0) || operation == "")
+            number1 = double.Parse(textBox1.Text);
+            if (isNumber || operation == "")
         {
-            // Якщо це перше число або після '='
             result = number1;
         }
             else
@@ -75,38 +57,27 @@ namespace Calculator
                 switch (operation)
                 {
                     case "/":
-                        textBox1.Text = "/";
                         if (number1 != 0)
                         {
                             result /= number1;
-                           // number1 = result;
                         }else {
                             MessageBox.Show("Nie wolno dzielić przez zero");
                         }
                         
                         break;
                     case "+":
-                        textBox1.Text = "+";
                         result += number1;
-                        //number1 = result;
                         break;
                     case "-":
-                        textBox1.Text = "-";
-                        result = number1 - number2;
-                        number1 = result;
-                        
+                        result -= number1;                        
                         break;
                     case "x":
-                        textBox1.Text = "x";
-                        result = number2 * number1;
-                        number1 = result;
+                        result *= number1;
                         break;
                 }
-
             }
-                isNumber = false;
+                isNumber = true;
                 operation = nextOperation;
-                operation = "";
             }
 
         
@@ -117,31 +88,17 @@ namespace Calculator
             //c,ce,,,+/-,=
             Button button = (Button)sender;
             if (button == null) return;
-            //double dn1, dn2, result;
-            //result = 0;
-
             switch (button.Text)
             {
                 case "=":
-                    isOperation = true;
-                    do_operation();
+                    do_operation("");
                     textBox1.Text = result.ToString();
-                    number = textBox1.Text;
-                    isNumber = true;
-                  
+                    operation = "";                  
                     break;
                 case ",":
                     if (!textBox1.Text.Contains(","))
                     {
-                        if (isNumber == false)
-                        {
-                            textBox1.Text = "0" + button.Text;
-                            number = textBox1.Text;
-                            isNumber = true;
-                        }
-                        else
                             textBox1.Text += ",";
-                            number = textBox1.Text;
                     }
                     break;
                 case "C":
@@ -152,7 +109,6 @@ namespace Calculator
 
                     break;
                 case "X":
-                    // Видаляємо останній символ
                     if (textBox1.Text.Length > 0)
                     {
                         textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
